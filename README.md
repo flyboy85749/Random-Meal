888b4917a81b63133d2855da9ca9ac2d025f32f9
codemouse <freedmouse@gmail.com>
2019-09-27 14:08:45 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Random-Meal
 Random meals at the push of a button
+
+## Quick Little Project
+Just a little side project using the meal db API, as described in a free code camp article
+Added a bulma css layout and my own styling, just to play around with APIs
+Will experiment later with other search functions available through meal db, and add user input
+
+## Screenshot
+<img src="assets/images/meal_db.png">
---
+++ assets/javascript/recipes.js
@@ -0,0 +1,91 @@
+// first, we'll assign the button to display the info from the API in the get-meal div
+const get_meal_btn = document.getElementById('get_meal');
+
+// then, the meal_container const will display the related items
+const meal_container = document.getElementById('meal');
+
+// Let's see what the API pulls in.
+// if we paste this url in our browser, we'll get information for one recipe; an apple franzipan tart.
+// https://www.themealdb.com/api/json/v1/1/random.php
+// if you refresh the page, you'll get another item. 
+// that's great. Now, let's connect it to our button.
+get_meal_btn.addEventListener('click', () => {
+    fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+        .then(res => res.json())
+        .then(res => {
+            createMeal(res.meals[0]);
+        })
+        .catch(e => {
+            console.warn(e);
+        });
+});
+
+// that's great. Now we need to pass that meal into our code
+const createMeal = meal => {
+    // create an empty array to store ingredients
+    const ingredients = [];
+
+    // Get all ingredients from the object. Up to 20
+    for (let i = 1; i <= 20; i++) {
+        if (meal[`strIngredient${i}`]) {
+            ingredients.push(
+                `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+            );
+        } else {
+            // Stop if there are no more ingredients
+            break;
+        }
+    }
+    const newInnerHTML = `
+		<div class="row-one">
+			
+            <div class="columns seven">
+            
+                <h4 class="meal-type">${meal.strMeal}</h4>
+               
+                
+                <p>${meal.strInstructions}</p>
+               
+            
+            </div>
+            <div class="row-two">
+            <div class="columns five">
+				<img src="${meal.strMealThumb}" alt="Meal Image">
+				${
+        meal.strCategory
+            ? `<ul><li><strong>Category:</strong> ${meal.strCategory}</li>`
+            : ''
+        }
+				${meal.strArea ? `<li><strong>Area:</strong> ${meal.strArea}</li>` : ''}
+				${
+        meal.strTags
+            ? `<li><strong>Tags:</strong> ${meal.strTags
+                .split(',')
+                .join(', ')}</li>`
+            : ''
+        }
+                <h5>Ingredients:</h5>
+                
+				
+					${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
+				</ul>
+			</div>
+		</div>
+		${
+        meal.strYoutube
+            ? `
+		<div class="row">
+			<h5>Video Recipe</h5>
+			<div class="videoWrapper">
+				<iframe width="420" height="315"
+				src="https://www.youtube.com/embed/${meal.strYoutube.slice(-11)}">
+				</iframe>
+			</div>
+		</div>`
+            : ''
+        }
+	`;
+
+    meal_container.innerHTML = newInnerHTML;
+};
+
